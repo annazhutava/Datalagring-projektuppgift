91a038d58a8c74456f5e19a92d78bc36cba22177
Marielle Lunnan <marielle.lunnan@learnet.se>
2021-11-23 13:45:16 +0100

    Preparing tests

---
+++ DataLayer/TestSuite/Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer.Data;
+
+namespace DataLayer.TestSuite
+{
+    public class Tests
+    {
+        public void ResetDatabase() //<---resettar databasen inför varje test?
+        {
+            using var ctx = new TrackingDb();
+            ctx.Database.EnsureDeleted();
+            ctx.Database.EnsureCreated();
+            ctx.Seed();
+        }
+    }
+}
