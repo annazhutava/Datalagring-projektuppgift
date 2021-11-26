316d6f0ce678ff6bbf4e4f0f7a6e8ed701da642a
Marielle Lunnan <marielle.lunnan@learnet.se>
2021-11-26 14:55:50 +0100

    påbörjad testsuite

--- TestProject1/TestSuite.cs
+++ TestProject1/TestSuite.cs
@@ -1,13 +1,20 @@
 using Xunit;
 
-namespace TestProject1
+namespace BackendTests
 {
     public class TestSuite
     {
+        public LoginTestSuite()
+        {
+            var database = new Database();
+            database.
+                ();
+        }
         [Fact]
         public void Test1()
         {
-
+            var login = new Login();
+            Assert.True(login.Attempt("username", "Password"));
         }
     }
 }
