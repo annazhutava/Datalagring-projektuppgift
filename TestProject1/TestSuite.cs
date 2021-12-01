55647c7d8cb5de5a16403a15758a530350c0e05d
Pontus <pontus.berg2@learnet.se>
2021-12-01 13:47:50 +0100

    UserBackEndTest

--- TestProject1/TestSuite.cs
+++ TestProject1/TestSuite.cs
@@ -1,20 +1,40 @@
+using DataLayer.Backend;
+using DataLayer.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using Xunit;
 
 namespace BackendTests
 {
     public class TestSuite
     {
-        public LoginTestSuite()
-        {
-            var database = new Database();
-            database.
-                ();
-        }
         [Fact]
-        public void Test1()
+        public void UserBackEndTest()
         {
-            var login = new Login();
-            Assert.True(login.Attempt("username", "Password"));
+            using var ctx = new TrackingDb();
+            var userTools = new UserBackend();
+            var adminTools = new AdminBackend();
+            adminTools.PrepDatabase();
+
+            var query = ctx
+                .LunchBoxes
+                .Where(l => l.Id == 2)
+                .Include(l => l.Customer);
+
+            var lunchbox = query.First();
+            
+            Assert.True(lunchbox.Customer == null);
+            
+            userTools.BuyLunchbox("pontusberg830@gmail.com", 2);
+            using var ctx2 = new TrackingDb();
+            query = ctx2
+                .LunchBoxes
+                .Where(l => l.Id == 2)
+                .Include(l => l.Customer);
+            lunchbox = query.First();
+            
+            Assert.Equal("pontusberg830@gmail.com", lunchbox.Customer.Email);
+
         }
     }
 }
